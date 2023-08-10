2d257b767ce07f2bbeccc693db6a215af000455d
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-08-10 17:06:28 +0330

    Build Calculator component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
-const App = () => <>Mathmagician</>;
+import Calculator from './Components/Calculator';
+
+const App = () => <><Calculator /></>;
 
 export default App;
---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,3 @@
+const Calculator = () => 'Calculator';
+
+export default Calculator;
