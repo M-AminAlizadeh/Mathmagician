a1b85c100b9a9f96ab130be069a82b1ca4ce7874
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-08-10 17:24:17 +0330

    Build Result component

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,3 +1,11 @@
-const Calculator = () => 'Calculator';
+import Button from './Button';
+import Result from './Result';
+
+const Calculator = () => (
+  <>
+    <Result value="0" />
+    <Button value="AC" />
+  </>
+);
 
 export default Calculator;
