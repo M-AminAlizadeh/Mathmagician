b646fb4c6f2a33ea2fed84446f9fe801fb2cb9e9
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-08-11 08:41:15 +0330

    Build initial structure of calculator btns

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,11 +1,16 @@
 import Button from './Button';
 import Result from './Result';
 
-const Calculator = () => (
-  <>
-    <Result value="0" />
-    <Button value="AC" />
-  </>
-);
+const Calculator = () => {
+  const btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-',
+    '1', '2', '3', '+', '0', '.', '='];
+  return (
+    <section className='calculator'>
+      <Result value="0" />
+      {btns.map((btn) => <Button key={btn} value={btn} />)}
+
+    </section>
+  );
+};
 
 export default Calculator;
