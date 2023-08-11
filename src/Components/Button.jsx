6b9c6434b4a8e0f5d0a56fb9840ae7c34a3d0528
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-11 21:12:14 +0330

    Link functionality of Logic folder to app

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ value, className }) => <button type="button" className={className}>{value}</button>;
+const Button = ({ value, className, onClick }) => <button type="button" className={className} onClick={onClick}>{value}</button>;
 
 Button.propTypes = {
   value: PropTypes.string,
   className: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
   value: '',
   className: '',
+  onClick: '',
 };
 
 export default Button;
--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,21 +1,44 @@
+import { useState } from 'react';
 import Button from './Button';
 import Result from './Result';
 import calculate from '../Logic/calculate';
 
 const Calculator = () => {
-  const btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'X', '4', '5', '6', '-',
+  const btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-',
     '1', '2', '3', '+', '0', '.', '='];
+  const [resultValue, setRresultValue] = useState('');
+
+  const handleClick = (e) => {
+    setRresultValue(calculate(resultValue, e.target.innerText));
+  };
+
+  const showCalculator = () => {
+    if (!resultValue.total && resultValue.next) {
+      return resultValue.next;
+    }
+    if (resultValue.total && !resultValue.next && resultValue.operation) {
+      return `${resultValue.total} ${resultValue.operation}`;
+    }
+    if (resultValue.total && resultValue.next && resultValue.operation) {
+      return `${resultValue.total} ${resultValue.operation} ${resultValue.next}`;
+    }
+    if (resultValue.total && !resultValue.next && !resultValue.operation) {
+      return resultValue.total;
+    }
+    return 0;
+  };
+
   return (
     <section className="calculator">
-      <Result value="0" />
+      <Result value={showCalculator()} />
       {btns.map((btn) => {
-        if (['÷', '-', '=', '+', 'X'].includes(btn)) {
-          return <Button key={btn} value={btn} className="btn border bg-2" />;
+        if (['÷', '-', '=', '+', 'x'].includes(btn)) {
+          return <Button key={btn} value={btn} className="btn border bg-2" onClick={(e) => handleClick(e)} />;
         }
         if (btn === '0') {
-          return <Button key={btn} value={btn} className="btn border zero-btn bg-1" />;
+          return <Button key={btn} value={btn} className="btn border zero-btn bg-1" onClick={(e) => handleClick(e)} />;
         }
-        return <Button key={btn} value={btn} className="btn border bg-1" />;
+        return <Button key={btn} value={btn} className="btn border bg-1" onClick={(e) => handleClick(e)} />;
       })}
     </section>
   );
