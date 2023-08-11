45b06f464b6f6dd14cef1343b5e337a3fa07749a
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-11 09:57:01 +0330

    Install propTypes

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,15 @@
-/* eslint-disable */
-const Button = ({ value }) => <button type="button">{value}</button>;
+import PropTypes from 'prop-types';
+
+const Button = ({ value, className }) => <button type="button" className={className}>{value}</button>;
+
+Button.propTypes = {
+  value: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Button.defaultProps = {
+  value: '',
+  className: '',
+};
 
 export default Button;
