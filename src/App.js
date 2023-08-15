9efc9825b999e264d5e3ee243fbfaa4d99e240e4
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-08-15 18:24:23 +0330

    Link different pages with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Calculator from './Components/Calculator';
-import Quote from './Components/Quote';
+import {
+  Routes, Route,
+} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Calculator from './Pages/Calculator';
+import Quote from './Pages/Quote';
+import Notfound from './Pages/Notfound';
 import './style.css';
 
 const App = () => (
-  <main className="container">
-    <Quote />
-    <Calculator />
-  </main>
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  </>
 );
 
 export default App;
